c008efa3251b8db2b2bdedec39fa77d564136162
Yusuf Bıyık <yusufbyk3660@gmail.com>
2021-05-20 21:05:32 +0300

    ⚡ Improve
    - Combined an if block with the one on top of it
    - total_listening_time now contains only the msPlayed data of every item
    instead of all of the item

--- listening_time_calculator.py
+++ listening_time_calculator.py
@@ -4,15 +4,14 @@ total_listening_time = [] # A variable to collect each milisecond of listening t
 
 try: # Going through each StreamingHistory file and gathering listening time data for each song
     for path in pathlib.Path('your-data').iterdir():
-        if path.is_file():
-            if path.name.startswith('StreamingHistory'):
-                print('Going through {}'.format(path.name))
-                currentFile = open(path, 'r', encoding='UTF-8')
-                for item in json.loads(currentFile.read()):
-                    total_listening_time.append(item)
-                currentFile.close()
+        if path.is_file() and path.name.startswith('StreamingHistory'):
+            print('Going through {}'.format(path.name))
+            currentFile = open(path, 'r', encoding='UTF-8')
+            for item in json.loads(currentFile.read()):
+                total_listening_time.append(item['msPlayed'])
+            currentFile.close()
 finally: # Calculating total listening time
     total = 0
-    for item in total_listening_time:
-        total += int(item['msPlayed'])
+    for ms in total_listening_time:
+        total += int(ms)
     print(f'Your total listening time is {total/3600000:.2f} hours which is {total/86400000:.2f} days!')
