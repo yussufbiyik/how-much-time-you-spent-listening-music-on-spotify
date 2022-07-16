c03d5fcd61d38da121b6dc6cc78e8bf8b5faed67
Yusuf Bıyık <25200573+yussufbiyik@users.noreply.github.com>
2022-07-16 15:14:35 +0300

    🆙 Add comment lines for readability

--- listening_time_calculator.py
+++ listening_time_calculator.py
@@ -8,10 +8,12 @@ try: # Going through each StreamingHistory file and gathering listening time dat
             print('Going through {}'.format(path.name))
             currentFile = open(path, 'r', encoding='UTF-8')
             for item in json.loads(currentFile.read()):
+                # Append the listening time to total_listening_time variable
                 total_listening_time.append(item['msPlayed'])
             currentFile.close()
 finally: # Calculating total listening time
     total = 0
     for ms in total_listening_time:
         total += int(ms)
+        # Divide the total to these specific numbers in order to get hour and day equivalent of the total miliseconds 
     print(f'Your total listening time is {total/3600000:.2f} hours which is {total/86400000:.2f} days!')
