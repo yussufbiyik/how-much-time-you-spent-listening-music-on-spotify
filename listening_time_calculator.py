4f55b7f3fbd11ae65b48d867444ef19a3815dc81
Yusuf Bıyık <yusufbyk3660@gmail.com>
2023-12-03 22:11:20 +0300

    ➕ Add detailed results
    - Add minutes, hours, days to results
    - Add support for both naming conventtions

--- listening_time_calculator.py
+++ listening_time_calculator.py
@@ -1,14 +1,20 @@
 import json, pathlib
+# import time
+# startTime = time.time()
 
 total = 0 # A variable to collect the total listening time
 
 try: # Going through each StreamingHistory file and gathering listening time data for each song
     for path in pathlib.Path('your-data').iterdir():
-        if path.is_file() and path.name.startswith('Streaming_History'):
+        # Look for both naming conventions of the file to take the new naming convention into account 
+        if path.is_file() and path.name.startswith(("Streaming_History", "StreamingHistory")):
             print('Going through {}'.format(path.name))
             currentFile = open(path, 'r', encoding='UTF-8')
             for item in json.loads(currentFile.read()):
-                total += int(item['ms_played'])
+                # Look for both naming conventions of the file to take the new naming convention into account 
+                total += int(item.get("msPlayed", item.get("ms_played", 0)))
             currentFile.close()
-finally: # Calculating total listening time
-    print(f'Your total listening time is {total/3600000:.2f} hours which is {total/86400000:.2f} days!')
+            # endTime = time.time()
+finally: # Calculating total listening time in minutes, hours and days
+    print(f'Your total listening time is {total/60000:.2f} minutes which is {total/3600000:.2f} hours which is {total/86400000:.2f} days!')
+    # print("This script took {:.5f} seconds to run.".format(endTime - startTime))
